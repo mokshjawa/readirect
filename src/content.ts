046b3f924a38c1139f3911e3317f4d562a94b6df
Moksh Jawa <mokshjawa@gmail.com>
2023-08-10 12:58:17 -0700

    [fix] fix type error on clicking element

--- src/content.ts
+++ src/content.ts
@@ -1,7 +1,6 @@
 // specific to archive.is (select first row's link)
-const link = document.querySelector("#CONTENT #row0 a");
+const link = <HTMLElement>document.querySelector("#CONTENT #row0 a");
 
 if (link) {
-  // @ts-expect-error
   link.click();
 }
